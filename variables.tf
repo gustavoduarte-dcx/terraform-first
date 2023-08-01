dc30e2f8a4c22201815b506a09d2158f3be04b61
Gustavo Duarte <132012030+gustavoduarte-dcx@users.noreply.github.com>
2023-08-01 14:10:41 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,19 +21,16 @@ variable "vpc_cidr_block" {
 
 variable "availability_zones" {
   description = "Número de zonas de disponibilidade"
-  type        = number
   default     = "<%=customOptions.AZs%>"
 }
 
 variable "public_subnet_count" {
   description = "Quantidade de subnets públicas"
-  type        = number
   default     = "<%=customOptions.publicSubnets%>"
 }
 
 variable "private_subnet_count" {
   description = "Quantidade de subnets privadas"
-  type        = number
   default     = "<%=customOptions.privateSubnets%>"
 }
 
